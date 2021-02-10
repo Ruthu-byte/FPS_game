318ff7a8d39ebc637670adcc7e6ba8e0a79be7fe
Sechaba <sechabantshehi@gmail.com>
2021-02-10 15:17:16 +0200

    Added the basic movement code

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public enum RotationAxes{
+        MouseX = 0,
+        MouseY = 1,
+        MouseXandY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXandY;
+
+    public float horSensitivity = 6.0f;
+    public float vertSensitivity = 6.0f;
+
+    public float vertMax = 45.0f;
+    public float vertMin = -45.0f;
+
+    private float _rotationX = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if(body != null){
+            body.freezeRotation = true;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(axes == RotationAxes.MouseX){
+            transform.Rotate(0, Input.GetAxis("Mouse X") * horSensitivity, 0);
+        }
+        else if(axes == RotationAxes.MouseY){
+            _rotationX -= Input.GetAxis("Mouse Y") * vertSensitivity;
+            _rotationX = Mathf.Clamp(_rotationX, vertMin, vertMax);
+
+            float rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else{
+            _rotationX -= Input.GetAxis("Mouse Y") * vertSensitivity;
+            _rotationX = Mathf.Clamp(_rotationX, vertMin, vertMax);
+
+            float delta = Input.GetAxis("Mouse X") * horSensitivity;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    public float speed = 24.0f;
+    public float gravity = -9.8f;
+    public float jumpSpeed = 35.0f;
+    public float terminalVelocity = -25.0f;
+    public float minFall = -1.5f;
+    public float dashSpeed = 60.0f;
+
+    private CharacterController _charController;
+    private float _vertSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        _vertSpeed = minFall;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        if(_charController.isGrounded){
+            if(Input.GetButtonDown("Jump")){
+                _vertSpeed = jumpSpeed;
+            }
+            else{
+                _vertSpeed = minFall;
+            }
+        }
+        else{
+            _vertSpeed += gravity * 5 * Time.deltaTime;
+            if(_vertSpeed < terminalVelocity){
+                _vertSpeed = terminalVelocity;
+            }
+        }
+
+        if(Input.GetButtonDown("Dash")){
+            movement *= dashSpeed;
+        }
+
+        movement.y = _vertSpeed;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+
+        _charController.Move(movement);
+    }
+}

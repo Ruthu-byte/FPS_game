7805e27d327a3fbef0446155d9a63aa546d805a6
Sechaba <sechabantshehi@gmail.com>
2021-02-16 00:27:57 +0200

    Added very good dash code

--- Assets/Scripts/Dash.cs
+++ Assets/Scripts/Dash.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Code comes form https://answers.unity.com/questions/824891/how-do-i-script-the-player-character-to-dash.html
+
 public class Dash : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Vector3 movementDirection;
+
+    public const float maxDashTime = 1.0f;
+    public float dashDistance = 10;
+    public float dashStoppingSpeed = 0.1f;
+    float currentDashTime = maxDashTime;
+    float dashSpeed = 6;
+    CharacterController controller;
+
+
+    private void Awake(){
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if(Input.GetButtonDown("Dash")){
+            currentDashTime = 0;
+        }
+        if(currentDashTime < maxDashTime){
+            movementDirection = transform.forward *dashDistance;
+            currentDashTime += dashStoppingSpeed;
+        }
+        else{
+            movementDirection = Vector3.zero;
+        }
+        controller.Move(movementDirection * Time.deltaTime * dashSpeed);
     }
 }
